18c469b7e077936b58ce7d5b6293e0e501a7ce97
Ben Beach <bbeach@innovated.tech>
2022-04-18 18:57:47 -0400

    didn't need to work on pagination at all prepping for launch to a dev server

--- customer/views.py
+++ customer/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.views import generic
 from .models import Customer
 
 
 # Create your views here.
-def all_customers(request):
-    return render(request, 'base.html')
+class AllCustomers(generic.ListView):
+    model = Customer
+    queryset = Customer.objects.all()
+    template_name = 'customer/all_customers.html'
--- wcadmin/views.py
+++ wcadmin/views.py
@@ -1,11 +1,9 @@
-from django.core.paginator import Paginator
-from django.shortcuts import render
+from datetime import datetime, timedelta
 from django.views import generic
 from customer.models import Customer
 
 
 class DashboardView(generic.ListView):
     model = Customer
-    queryset = Customer.objects.all()
-    paginate_by = 20
+    queryset = Customer.objects.filter(next_service__range=[datetime.today(), datetime.today() + timedelta(days=30)])
     template_name = "base.html"
