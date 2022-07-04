6676ae6ace031490fd035c557fe01be6fdb5e6a2
Ben Beach <bbeach@innovated.tech>
2022-07-03 21:52:55 -0500

    removing mysql

--- wcadmin/environment/production.py
+++ wcadmin/environment/production.py
@@ -16,14 +16,6 @@ SESSION_COOKIE_SECURE = True
 CONN_MAX_AGE = 20
 
 DATABASES = {
-    'mysql': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USERNAME'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
-    },
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get('PG_DB_NAME'),
